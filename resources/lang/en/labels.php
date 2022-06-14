89f5f8382ffaf5ecb16ed0a48091db9d1a631543
Shazabch <89830671+Shazabch@users.noreply.github.com>
2022-06-14 18:07:44 +0500

    First Commit with multi auth and theme implentation

---
+++ resources/lang/en/labels.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember_me' => 'Remember Me', 
+    'forget_password' => 'Forget Your Password ?',
+    'login' => 'Sign In' ,
+    'invoices' => 'Invoices',
+    'view_invoice' => 'View All Invoices' ,
+    'add_invocie' => 'Add New Invoice' ,
+    'customers' => 'Customers',
+    'view_customer' => 'View All Customers' ,
+    'add_customer' => 'Add New Customer' ,
+    'payments' => 'Payments' ,
+    'view_payment' => 'View All Payments' ,
+    'add_payment' => 'Add New Payment' ,
+    'users' => 'Admin Users' ,
+    'view_user' => 'View All Users' ,
+    'add_user' => 'Add New User' ,
+    'settings' => 'Settings' ,
+    'change_password' => 'Change Password' ,
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,58 +1,84 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-        <a href="/">
-         <img style="margin-left: 80px;" src="{{asset('images/BMI_Group_Logo.jpg')}}" width="80px"  alt="">
-                
-            </a><br>
-            <b style="font-size: 30px;">Customer Login</b>
-        </x-slot>
+@extends('layouts.auth-login')
+@section('content')
+<main class="main-content  mt-0">
+    <section>
+      <div class="page-header min-vh-75">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+              <div class="card card-plain mt-8">
+                <div class="card-header pb-0 text-left bg-transparent">
+                  <h3 class="font-weight-bolder text-info text-gradient">Welcome back Customer</h3>
+                  <p class="mb-0">Enter your email and password to sign in</p>
+                </div>
+                <div class="card-body">
+                    @if(session('status'))
+                        <div class="alert alert-success">
+                            {{session('status')}}
+                        </div>
+                    @endif
+                    @if($errors->any())
+                        @foreach($errors->all() as $error)
+                            <div style="font-size: 12px;" class=" text-danger">
+                                {{$error}}
+                            </div>
+                        @endforeach
+                    @endif
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+                  <form role="form" method="POST" action="{{ route('login') }}">
+                    @csrf
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                    <label>{{__('labels.email')}}</label>
+                    <div class="mb-3">
+                      <input  class="form-control" 
+                               placeholder="Email" 
+                               aria-label="Email" 
+                               type="email" 
+                               name="email" 
+                               :value="old('email')" 
+                               required autofocus>
+                    </div>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
+                    <label>{{__('labels.password')}}</label>
+                    <div class="mb-3">
+                      <input  type="password"
                                 name="password"
-                                required autocomplete="current-password" />
-            </div>
+                                required autocomplete="current-password" 
+                                 class="form-control" 
+                                 placeholder="Password" 
+                                 aria-label="Password" 
+                                 aria-describedby="password-addon">
+                    </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                    <div class="form-check form-switch">
+                      <input class="form-check-input" type="checkbox" id="remember_me" checked="">
+                      <label class="form-check-label" for="rememberMe">{{__('labels.remember_me')}}</label>
+                    </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+                    <div class="text-center">
+                      <button type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">{{__('login')}}</button>
+                    </div>
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
+                  </form>
+                </div>
+                <div class="card-footer mx-4 pt-0 px-lg-2 px-1">
+                  <p class="mb-4 text-sm mx-auto">
+                  @if (Route::has('password.request'))
+                   <a href="{{ route('password.request') }}">{{__('labels.forget_password')}}</a>
+                  @endif
+                  <a href="{{ route('register') }}" class="text-info text-gradient font-weight-bold">Sign Up</a>
+                  </p>
+                </div>
+              </div>
             </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+            <div class="col-md-6">
+              <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+                <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6"  style="background-image:url('{{asset('soft-theme/assets/img/curved-images/curved6.jpg')}}')"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+@endsection
